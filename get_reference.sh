a4aa3e3e0007e78ba3886b2d062d2ae5e9d10207
Angela Aeschbach <aaeschbach@binfservms01.unibe.ch>
2020-11-11 16:32:13 +0100

    download genome and annotation then CHECKSUMS

---
+++ get_reference.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Download files
+wget 'ftp://ftp.ensembl.org/pub/release-101/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz'
+wget 'ftp://ftp.ensembl.org/pub/release-101/fasta/mus_musculus/dna/CHECKSUMS'
+mv CHECKSUMS CHECKSUMS_fasta
+
+
+wget 'ftp://ftp.ensembl.org/pub/release-101/gtf/mus_musculus/Mus_musculus.GRCm38.101.gtf.gz'
+wget 'ftp://ftp.ensembl.org/pub/release-101/gtf/mus_musculus/CHECKSUMS'
+mv CHECKSUMS CHECKSUMS_gtf
+
+
+# Check if downloaded files are intact 
+sum Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
+grep Mus_musculus.GRCm38.dna.primary_assembly.fa.gz CHECKSUMS_fasta
+
+
+sum Mus_musculus.GRCm38.101.gtf.gz
+grep Mus_musculus.GRCm38.101.gtf.gz CHECKSUMS_gtf
