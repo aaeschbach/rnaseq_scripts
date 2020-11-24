b3910424188a612c5165a4b98e6285047cd09342
Angela Aeschbach <aaeschbach@binfservms01.unibe.ch>
2020-11-24 14:26:07 +0100

    modified fastqc_script.sh

--- fastqc_script.sh
+++ fastqc_script.sh
@@ -3,23 +3,19 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1000M
 #SBATCH --time=01:00:00
-#SBATCH --job-name=quality_checks
+#SBATCH --job-name=fastqc
 #SBATCH --mail-user=angela.aeschbach@students.unibe.ch
 #SBATCH --mail-type=begin,end
 #SBATCH --partition=pcourse80
-#SBATCH --output=/home/aaeschbach/aaeschbach/1_quality_checks/output_%j.o
-#SBATCH --error=/home/aaeschbach/aaeschbach/1_quality_checks/error_%j.e
+#SBATCH --output=/data/courses/rnaseq/toxoplasma_de/aaeschbach/output_fastqc.o
+#SBATCH --error=/data/courses/rnaseq/toxoplasma_de/aaeschbach/error_fastqc.e
+#SBATCH --array=0-31
 
 module add UHTS/Quality_control/fastqc/0.11.7
 
-SAMPLE_NAMES=(SRR7821920 SRR7821938 SRR7821950 SRR7821968)
+FILES=(/data/courses/rnaseq/toxoplasma_de/reads/*)
 
-for sample in ${SAMPLE_NAMES[@]}
-do
-read_1=/data/courses/rnaseq/toxoplasma_de/reads/${sample}_1.fastq.gz
-read_2=/data/courses/rnaseq/toxoplasma_de/reads/${sample}_2.fastq.gz
-output_dir=/home/aaeschbach/aaeschbach/1_quality_checks/${sample}
-mkdir $output_dir
-fastqc --outdir=$output_dir $read_1 $read_2
-done
+OUTPUT_DIR=/data/courses/rnaseq/toxoplasma_de/aaeschbach/1_fastqc/
+mkdir $OUTPUT_DIR
 
+fastqc --outdir=$OUTPUT_DIR ${FILES[$SLURM_ARRAY_TASK_ID]}
