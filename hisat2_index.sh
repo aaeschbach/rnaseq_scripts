0c66f54b56d516a8af34d78cee7684f1f08bae53
Angela Aeschbach <aaeschbach@binfservms01.unibe.ch>
2020-11-25 16:00:22 +0100

    updated hisat2_index.sh with name and release of reference genome

--- hisat2_index.sh
+++ hisat2_index.sh
@@ -7,18 +7,18 @@
 #SBATCH --mail-user=angela.aeschbach@students.unibe.ch
 #SBATCH --mail-type=begin,end
 #SBATCH --partition=pcourse80
-#SBATCH --output=/home/aaeschbach/aaeschbach/2_map_reads/output_%j.o
-#SBATCH --error=/home/aaeschbach/aaeschbach/2_map_reads/error_%j.e
+#SBATCH --output=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/output_%j.o
+#SBATCH --error=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/error_%j.e
 
 module add UHTS/Aligner/hisat/2.2.1
 
 mkdir hisat2_index
 
+# Mus musculus genome Ensembl GRCm38 (release 101)
 gunzip Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
 
-FASTA=Mus_musculus.GRCm38.dna.primary_assembly.fa
+REF_GENOME=Mus_musculus.GRCm38.dna.primary_assembly.fa
 HT2_INDEX_BASE=hisat2_index/Mus_musculus.GRCm38
 
-
 # Build index
-hisat2-build -p 1 ${FASTA} ${HT2_INDEX_BASE}
+hisat2-build -p 1 ${REF_GENOME} ${HT2_INDEX_BASE}
