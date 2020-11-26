2aefbbe94fd5fa971553a1e9c1078f57cb5083b3
Angela Aeschbach <aaeschbach@binfservms01.unibe.ch>
2020-11-26 17:20:31 +0100

    add scripts for hisat2 (mapping) and samtools (view, sort, index)

---
+++ samtools_sort.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=25000M
+#SBATCH --time=01:00:00
+#SBATCH --job-name=samtools_sort
+#SBATCH --mail-user=angela.aeschbach@students.unibe.ch
+#SBATCH --mail-type=begin,end
+#SBATCH --partition=pcourse80
+#SBATCH --output=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/output_%j.o
+#SBATCH --error=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/error_%j.e
+#SBATCH --array=0-3
+
+# Sample names passed as arguments stored here
+SAMPLES=("$@")
+BAM_DIR=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/bam_files/
+OUT_DIR=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/sorted_bam_files/
+mkdir $OUT_DIR
+
+# Sort bam files by genomic coordinates
+module add UHTS/Analysis/samtools/1.10
+
+BAM_FILE=${BAM_DIR}${SAMPLES[$SLURM_ARRAY_TASK_ID]}.bam
+SORTED_BAM_FILE=${OUT_DIR}${SAMPLES[$SLURM_ARRAY_TASK_ID]}.sorted.bam
+
+samtools sort -m 25000M -@ 4 -o ${SORTED_BAM_FILE} -T temp ${BAM_FILE}
---
+++ samtools_view.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4000M
+#SBATCH --time=01:00:00
+#SBATCH --job-name=samtools_view
+#SBATCH --mail-user=angela.aeschbach@students.unibe.ch
+#SBATCH --mail-type=begin,end
+#SBATCH --partition=pcourse80
+#SBATCH --output=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/output_%j.o
+#SBATCH --error=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/error_%j.e
+#SBATCH --array=0-3
+
+# Sample names passed as arguments stored here
+SAMPLES=("$@")
+SAM_DIR=/data/courses/rnaseq/toxoplasma_de/mapping/
+OUT_DIR=/data/courses/rnaseq/toxoplasma_de/aaeschbach/2_map_reads/bam_files/
+mkdir $OUT_DIR
+
+# Convert sam files to bam format
+module add UHTS/Analysis/samtools/1.10
+
+SAM_FILE=${SAM_DIR}${SAMPLES[$SLURM_ARRAY_TASK_ID]}.sam
+BAM_FILE=${OUT_DIR}${SAMPLES[$SLURM_ARRAY_TASK_ID]}.bam
+
+samtools view -hbS ${SAM_FILE} > ${BAM_FILE}
